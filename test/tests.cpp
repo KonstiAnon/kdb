03afeb94d3ed7b79eecbe048cb46f04f591f4de1
Konstantin Kissel <konstantin.kissel@tum.de>
2025-06-30 21:57:35 +0200

    added first real tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,7 +3,23 @@
 //
 
 #include <catch2/catch_test_macros.hpp>
+#include <libkdb/process.hpp>
+#include <libkdb/error.hpp>
 
-TEST_CASE("validate environment"){
-    REQUIRE(false);
+using namespace kdb;
+
+namespace {
+    bool process_exists(pid_t pid){
+        auto ret = kill(pid, 0);
+        return ret != -1 and errno != ESRCH;
+    }
+}
+
+TEST_CASE("process::launch success", "[process]") {
+    auto proc = process:launch("yes");
+    REQUIRE(process_exists(proc->pid()));
+}
+
+TEST_CASE("process::launch no such program", "[process]") {
+    REQUIRE_THROWS_AS(process::launch("fake_program_non_existent"), error);
 }
